b82deded1f515536b86d87f23f6fd8f944c389b5
mark5600@gmail.com <mark5600@gmail.com@3d276773-70ad-0bf4-bc9c-8df326406ef2>
2012-02-10 03:05:51 +0000

    Dempster Shafer combiner

--- trunk/src/com/inca/main/Ensemble.java
+++ trunk/src/com/inca/main/Ensemble.java
@@ -4,6 +4,7 @@ package com.inca.main;
 import static com.googlecode.javacv.cpp.opencv_core.cvLoad;
 
 import java.io.File;
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
@@ -47,11 +48,12 @@ public class Ensemble {
 		// Essentially an average of all algorithms' performance for each symbol,
 		// each CV is compared against this for the decision template combiner
 	private static double[] decision_templates; 
+	private static final int SYMBOL_SPACE_SZ = 10;
 	
 	static{
 		if (!trained){
 			try{
-				decision_templates = new double[10];
+				decision_templates = new double[SYMBOL_SPACE_SZ];
 				trainDP();
 				trained = true;
 			}
@@ -89,14 +91,14 @@ public class Ensemble {
 					double divisor = 0.0;
 					// each column (symbol recognized)
 					// sum [row][j]
-					for (int j=0; j<10; j++){
+					for (int j=0; j<SYMBOL_SPACE_SZ; j++){
 						divisor += pm.get(i, j);
 					}
 					
 					
 					ConfidenceVector cv = new ConfidenceVector(String.valueOf(i));
 					// Loop back through, compute posterior probability
-					for (int j=0; j<10; j++){
+					for (int j=0; j<SYMBOL_SPACE_SZ; j++){
 						double post = pm.get(i,j) / divisor;
 						cv.addElement(post);
 					}
@@ -112,7 +114,7 @@ public class Ensemble {
 			}
 			
 			// Finally, average CV value for each symbol and put into decision template
-			for (int i=0; i < 10; i++){
+			for (int i=0; i < SYMBOL_SPACE_SZ; i++){
 				List<ConfidenceVector> cvs = symbolToCvs.get(i);
 				double weight = 0.0;
 				for (int j=0; j<cvs.size(); j++)
@@ -139,7 +141,7 @@ public class Ensemble {
 		int avgComb = averagingCombiner();
 		System.out.println("averaging combiner says " + avgComb);
 		System.out.println("decision template says " + decisionTemplateCombiner());
-	
+		System.out.println("demp shaf says " + dempsterShaferCombiner());
 		return avgComb;
 	}//end getDecision method
 
@@ -172,6 +174,106 @@ public class Ensemble {
 //			choices.add(new CombinerChoice(i, distance));
 //		}
 		return Collections.min(choices).getSymbol();
+	}
+	
+	// With C = number of symbols
+	//		L = number of classifiers
+	// Proximity (C x L) --> Degrees of Belief (C x L) --> Support (C)
+	private int dempsterShaferCombiner(){
+		if (!trained)
+			return -1;
+		
+		// Calculate proximity from decision template 
+		// to each classifier
+		ArrayList<DempShaf>[] symbolToProx = new ArrayList[SYMBOL_SPACE_SZ];
+		
+		for (int i=0; i < SYMBOL_SPACE_SZ; i++){
+			for (int j=0; j< confVects.size(); j++){
+				ConfidenceVector cv = confVects.get(j);
+				double proximity = 0.0;
+				final double output = cv.getElement(i);
+				// Numerator: distance to this symbol's decision template
+				proximity = Math.pow(output - decision_templates[i], 2);
+				proximity += 1;
+				proximity = Math.pow(proximity, -1);
+				double denom = 0.0;
+				// Denominator: sum of distance to all decision templates
+				for (int k=0; k<decision_templates.length; k++){
+					denom += Math.pow(Math.pow(output - decision_templates[k],2), -1);
+				}
+				proximity /= denom;
+				
+				// Each decision template symbol has a list of proximities (length L)
+				// attached to it
+				if (symbolToProx[i] == null)
+					symbolToProx[i] = new ArrayList<DempShaf>();
+				
+				symbolToProx[i].add(new DempShaf(i, proximity));
+			}
+		}
+		
+		
+		
+		// Generate degrees of belief for every classifier for every symbol
+		for (int i=0; i<symbolToProx.length; i++){
+			ArrayList<DempShaf> dsList = (ArrayList<DempShaf>)symbolToProx[i];
+			for (int j=0; j<dsList.size(); j++){
+				DempShaf ds = dsList.get(j);
+				double belief = 0.0;
+				belief = ds.getProx();
+				double product = 0.0;
+				// What did the other classifiers for this symbol say?
+				for (int k=0; k<dsList.size(); k++){
+					if (dsList.get(k) != ds) 
+						product = product > 0.0 ? product*dsList.get(k).getProx() : dsList.get(k).getProx();
+				}
+				belief *= product;
+				// Denominator
+				belief /= ( (1 - ds.getProx()) * (1 - product) ) ;
+				
+				ds.setBelief(belief);
+			}
+		}
+		
+		// For each symbol, calculate support
+		List<CombinerChoice> choices = new ArrayList<CombinerChoice>();
+		for (int i=0; i<symbolToProx.length; i++){
+			ArrayList<DempShaf> dsList = (ArrayList<DempShaf>)symbolToProx[i];
+			double support = 0.0;
+			for (int j=0; j<dsList.size(); j++){
+				support = support > 0.0 ? support*dsList.get(j).getBelief() : dsList.get(j).getBelief();
+			}
+			choices.add(new CombinerChoice(i, support));
+		}
+		
+		return Collections.min(choices).getSymbol();
+	}
+	
+	private class DempShaf{
+		private double prox, belief;
+		private int ix; // Symbol
+		
+		DempShaf(int ix){
+			this(ix, 0.0, 0.0);
+		}
+		
+		DempShaf(int ix, double prox){
+			this(ix, prox, 0.0);
+		}
+		
+		DempShaf(int ix, double prox, double belief){
+			this.prox = prox;
+			this.belief = belief;
+			this.ix = ix;
+		}
+		
+		public double getProx() {return prox;}
+		public void setProx(double prox) {this.prox = prox;}
+		public double getBelief() {return belief;}
+		public void setBelief(double belief) {this.belief = belief;}
+		public int getIx() {return ix;}
+		public void setIx(int ix) {this.ix = ix;}
+
 	}
 	
 	private class CombinerChoice implements Comparable<CombinerChoice>{
