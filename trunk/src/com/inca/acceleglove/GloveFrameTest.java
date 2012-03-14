f72a3f48b7acce144ca6005bd5c7f5d287e03f13
jimbolysses <jimbolysses@3d276773-70ad-0bf4-bc9c-8df326406ef2>
2012-03-14 20:39:12 +0000

    modifications for cross validation runs

--- trunk/src/com/inca/acceleglove/GloveFrameTest.java
+++ trunk/src/com/inca/acceleglove/GloveFrameTest.java
@@ -1,12 +1,19 @@
 package com.inca.acceleglove;
 
+import static com.googlecode.javacv.cpp.opencv_core.CV_32FC1;
+import static com.googlecode.javacv.cpp.opencv_core.cvCreateMat;
+
+import com.googlecode.javacv.cpp.opencv_core.CvMat;
 import com.idrt.Glove;
 import com.idrt.GloveConnectionEvent;
 import com.idrt.GloveConnectionListener;
 import com.idrt.Handshape;
+import com.inca.main.IncaDecision;
+import com.inca.main.PerformanceMatrix;
 
 public class GloveFrameTest implements GloveConnectionListener{
 	private AlgFrame frame;
+	protected static final int ALPH_SIZE = 10;
 	
 	public GloveFrameTest()throws Exception{
 		frame = new AlgFrame();
@@ -23,7 +30,8 @@ public class GloveFrameTest implements GloveConnectionListener{
 	
 	public static void main(String[] args){
 		try {
-			new GloveFrameTest();
+			GloveFrameTest test = new GloveFrameTest();
+			test.crossVal(1);
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -37,4 +45,47 @@ public class GloveFrameTest implements GloveConnectionListener{
 		System.out.println("The Glove is disconnected");
 		System.exit(0);
 	}
-}//end GLoveGrameTest class
+	
+	public void crossVal(int rNum)throws Exception{
+		PerformanceMatrix pm1 = new PerformanceMatrix("GlKNNcv"+rNum, ALPH_SIZE);
+		PerformanceMatrix pm2 = new PerformanceMatrix("GlSVMcv"+rNum, ALPH_SIZE);
+		IncaDecision run = new IncaDecision();
+		KNearest knn = new KNearest(5);
+		SupportVM svm = new SupportVM();
+		GestureData hOut;
+		CvMat hArry;
+		
+		knn.train(frame.getKnnData("gloveset"), frame.getClassLabels());
+		svm.train(frame.getKnnData("gloveset"), frame.getClassLabels());
+		
+		for(int i = 0; i < 51; i++){
+			for(int j = 0; j < 10; j++){
+				run = new IncaDecision(null, null, null, null, run.getPrefixName(j));
+				hOut = new GestureData(getData("glovesetjhn", i));
+				hArry = hOut.toCvMat();
+				
+				run.updatePM(pm1, ""+knn.predict(hArry));
+				pm1.saveDatabase(pm1.getName()+".xml");
+				
+	    		hArry = hOut.toCvMat1();
+				run.updatePM(pm2, ""+svm.predict(hArry));
+				pm2.saveDatabase(pm2.getName()+".xml");
+			}
+		}
+		
+		run.outputPMs("GlKNNcv1");
+		run.outputPMs("GlSVMcv1");		
+	}
+	
+	private String getData(String filename, int indx)throws Exception{
+		String out = new String();
+		
+		CvMat set = frame.getKnnData(filename);
+		for(int i = 0; i < 18; i++){
+			out = out.concat((int)set.get(indx, i)+" ");
+		}
+		
+		return out;
+	}
+	
+}//end GloveGrameTest class
